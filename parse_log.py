edd31ec3390323f5c14d2376ac5c18630ee0595f
anatolijcurikov <kazay@mail.ru>
2022-12-17 16:34:50 +0300

    bug fixed

--- parse_log.py
+++ parse_log.py
@@ -7,29 +7,29 @@ import glob
 
 file_lst = glob.glob('/opt/DemonX_scripts/logs/errors_*')
 start_time_file = "/home/kazay/start_time"
-# log_file = "/opt/DemonX_scripts/logs/errors_moisklad_to_fulllog.log"
+
 
 def sendMessage(message):
     TOKEN = "5989022565:AAHO5SOwdlpdMdAFXWXtJxdli4WXy8XyYW8"
     chat_id = "-1001604614259"
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={message}"
-    print(requests.get(url).json()) # this sends the message
+    print(requests.get(url).json())  # this sends the message
 
 
 def write_start_script_time(file_location=start_time_file):
     with open(start_time_file, mode='w') as file:
-        file.write('last start at %s.' %(datetime.datetime.now() + datetime.timedelta(hours=3)))
+        file.write('last start at %s.' %(datetime.datetime.now() + datetime.timedelta(hours = 3)))
 
 
 def parse_time_from_line(line):
-    date_extract_pattern = "(?:\\d{4})-(?:\\d{2})-(?:\\d{2}).(?:\\d{2}):(?:\\d{2}):(?:\\d{2})" #(?:\\.\\d*)?)(?:(?:-(?:\\d{2}):(?:\\d{2}))?
+    date_extract_pattern = "(?:\\d{4})-(?:\\d{2})-(?:\\d{2}).(?:\\d{2}):(?:\\d{2}):(?:\\d{2})" 
     time = re.findall(date_extract_pattern,line)
     if len(time) != 0:
         return time[0]
 
 
 def compare_date(start_time, end_time):
-    if end_time != None:
+    if end_time is not None:
         if time.strptime(start_time, '%Y-%m-%d %H:%M:%S') <= time.strptime(end_time, '%Y-%m-%d %H:%M:%S'):
             return True
         else:
